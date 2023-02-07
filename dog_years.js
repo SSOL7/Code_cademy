0a5c5d5595f92c6ba126c4983dc27d383fd95bd6
Shahosol <soltani.shaho@gmail.com>
2023-02-07 18:13:02 +0200

    starting Codecademy journey

---
+++ dog_years.js
@@ -0,0 +1,10 @@
+let myAge = 31;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Shaho'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
